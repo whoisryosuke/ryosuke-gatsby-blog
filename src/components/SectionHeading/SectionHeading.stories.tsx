512bd7a59ca302f5ac2548f6b405b5982066d03b
Ryosuke <ryosuke.san.hana@gmail.com>
2019-10-07 08:06:14 -0700

    :zap: Section heading

---
+++ src/components/SectionHeading/SectionHeading.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import SectionHeading from "./index"
+
+export default { title: 'Section Heading' };
+
+export const basic = () => <SectionHeading emoji="📓" heading="Latest writings" />
