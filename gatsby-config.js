72590282332b5f1868d93cba9f8b0181195e2ed6
Ryosuke <ryosuke.san.hana@gmail.com>
2019-10-07 15:35:09 -0700

    :wrench: deprecated image feature dropped

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,6 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1080,
-              sizeByPixelDensity: true,
             },
           },
         ],
