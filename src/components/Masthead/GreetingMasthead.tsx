8a4420b15750e4ef15d30968a946f509d74c912d
Ryosuke <ryosuke.san.hana@gmail.com>
2019-10-07 08:05:26 -0700

    :zap: New masthead

---
+++ src/components/Masthead/GreetingMasthead.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import styled from "styled-components"
+import { Box, Heading } from 'rebass/styled-components'
+import MastheadSVG from '../../assets/svg/masthead-circles.svg'
+
+interface IGreetingMastheadProps {
+  className: string
+}
+
+const GreetingMasthead: React.FunctionComponent<IGreetingMastheadProps> = ({className}) => {
+  return(
+  <section className={className}>
+    <Box mb={4}>
+      <Heading variant="label" textAlign="right" mb={2}>はじめまして</Heading>
+        <Heading
+          fontSize={[3, 4, 7]} textAlign="right">
+          Hey I'm Ryosuke ✌️<br />
+          Designer, developer,<br />
+          &amp; influencer 💭
+      </Heading>
+    </Box>
+    <Box mb={4}>
+      <Heading variant="label" textAlign="right">わたし</Heading>
+        <Heading
+          fontSize={[2, 3, 5]} textAlign="right">
+          full stack engineer<br />
+          UI/UX designer 
+      </Heading>
+    </Box>
+    <Box mb={4}>
+        <Heading
+          fontSize={[2, 3, 5]} textAlign="right" mb={2}>
+          Los Angeles
+      </Heading>
+      <Heading variant="label" textAlign="right">`に住んでいる</Heading>
+    </Box>
+    <Box mb={4}>
+      <Heading variant="label" textAlign="right" mb={2}>I live for </Heading>
+        <Heading
+          fontSize={[1,2]} textAlign="right">
+          clean design, <br />
+          accessible UX,<br />
+          &amp; cutting-edge tech
+      </Heading>
+    </Box>
+  </section>
+  )
+};
+
+
+const StyledGreetingMasthead = styled(GreetingMasthead)`
+  padding:4rem;
+  border-bottom:1px solid ${(props) => props.theme.colors.black};
+
+  background-image:url(${MastheadSVG});
+  background-repeat:no-repeat;
+  background-size:60%;
+  background-position:10% 10%;
+
+  ${(props) => props.theme.mediaQueries.mobile} {
+    background-size:150%;
+    background-position:-30% 30%;
+  }
+
+  ${(props) => props.theme.mediaQueries.tablet} {
+    background-size:60%;
+  }
+`
+
+export default StyledGreetingMasthead;
--- src/components/Masthead/Masthead.stories.tsx
+++ src/components/Masthead/Masthead.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Masthead from "./index"
+import GreetingMasthead from "./GreetingMasthead"
 
 export default { title: 'Masthead' };
 
 export const basic = () => <Masthead />
+export const greeting = () => <GreetingMasthead />
