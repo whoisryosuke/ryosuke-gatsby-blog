e84a28a5f77b08586eecdcecf19b885a1499a54c
Ryosuke <ryosuke.san.hana@gmail.com>
2019-10-08 12:08:27 -0700

    :zap: cards can now be linked

--- src/components/Card/BaseCard.tsx
+++ src/components/Card/BaseCard.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
+import Link from 'gatsby-link'
 import styled from 'styled-components'
 import { Box } from 'rebass/styled-components'
 
+const StyledLink = styled(Link)`
+    text-decoration:none;
+    color:${(props) => props.theme.colors.black}
+`
+
 const StyledCard = styled(Box)`
+  border-width:1px 1px 1px 1px;
+  border-style: solid;
+  border-color: ${(props) => props.theme.colors.black};
   box-shadow:none;
-  border:1px solid #111212;
   padding:2.5em;
   background: linear-gradient(180deg, ${(props) => props.theme.colors.white} 0%, ${(props) => props.theme.colors.white} 100%);
   position:relative;
   z-index:10;
 
   transition: all 200ms linear;
-
 
   ${(props) => props.hover && `
     &:hover {
@@ -41,13 +48,31 @@ const StyledCard = styled(Box)`
       z-index:10;
     }
   `}
+
+  ${(props) => props.grid && `
+  ${(props) => props.theme.mediaQueries.tablet} {
+    border-width:0 0 1px 0;
+  }
+
+  &:last-child {
+    border-width:0 0 1px 1px;
+  }`}
 `
 
 interface IBaseCardProps {
   children: JSX.Element
 }
 
-const BaseCard: React.FunctionComponent<IBaseCardProps> = ({children, ...props}) => {
+const BaseCard: React.FunctionComponent<IBaseCardProps> = ({children, link, ...props}) => {
+  if(link) {
+    return(
+      <StyledCard {...props}>
+        <StyledLink to={link}>
+          {children}
+        </StyledLink>
+      </StyledCard>
+    )
+  }
   return(
     <StyledCard {...props}>
       {children}
--- src/components/Card/BasicCard.tsx
+++ src/components/Card/BasicCard.tsx
@@ -5,13 +5,20 @@ import BaseCard from "./BaseCard"
 interface IBasicCardProps {
   title: string,
   subheader: string,
-  description: string
+  description: string,
+  link: string
 }
 
-const BasicCard: React.FunctionComponent<IBasicCardProps> = ({title, subheader, description, ...props}) => {
+const BasicCard: React.FunctionComponent<IBasicCardProps> = ({title, subheader, description, link, ...props}) => {
+  // Check if Subheader is array or string
+  // return one item if array
+  let subtitle = subheader
+  if (Array.isArray(subheader) && subheader.length > 0) {
+    subtitle = subheader[Math.floor(Math.random() * subheader.length)]
+  }
   return(
-    <BaseCard {...props}>
-      <Heading variant="label" mb="4">{subheader}</Heading>
+    <BaseCard link={link} {...props}>
+      <Heading variant="label" mb="4">{subtitle}</Heading>
       <Heading variant="header" mb="4">{title}</Heading>
       <Text variant="paragraph">{description}</Text>
     </BaseCard>
