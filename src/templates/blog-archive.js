426715b3677cbdf7f6d533f2253f82353d2bc419
Ryosuke <ryosuke.san.hana@gmail.com>
2019-10-08 12:57:39 -0700

    :lipstick: Styled archive + pagination

--- src/templates/blog-archive.js
+++ src/templates/blog-archive.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
-import Link from "gatsby-link";
+import styled from 'styled-components'
+import GLink from "gatsby-link";
+import { Flex, Box, Text, Link as RLink } from 'rebass/styled-components'
 
 import capitalizeFirstLetter from '../helpers/uppercase';
 
 import Layout from "../layouts/BaseLayout"
+import SectionHeading from '../components/SectionHeading/SectionHeading';
 import PostLoop from '../components/PostLoop';
+
+const StyledLink = styled(GLink)`
+    text-decoration:none;
+    color:${(props) => props.theme.colors.black};
+`
+
+const StyledBox = styled(Box)`
+    border-bottom: 1px solid black;
+    background:${(props) => props.theme.colors.white};
+    transition: background 400ms ease-in;
+
+    &:hover, &:focus {
+        background:${(props) => props.test ? props.theme.colors.gray : props.theme.gradients.subtle};
+    }
+`
 
 const NavLink = props => {
     if (!props.test) {
-        return <Link to={props.url} className="btn">{props.text}</Link>;
+        return <StyledLink to={props.url}><Text variant="label">{props.text}</Text></StyledLink>;
     } else {
-        return <span className="btn disabled gray">{props.text}</span>;
+        return <Text color="gray" variant="label">{props.text}</Text>;
     }
 };
 
@@ -21,21 +39,18 @@ const IndexPage = ({ data, pathContext }) => {
 
     return (
         <Layout className="BlogArchive">
-            <header className="container Title">
-                <h1>{ capitalizeFirstLetter(pathPrefix) } archive</h1>
-                <h4 className="Title__sub">Page {index}</h4>
-            </header>
+            <SectionHeading emoji="📓" heading={`${capitalizeFirstLetter(pathPrefix)} archive`} subheader={`Page ${index}`} />
 
-            <PostLoop loop={group} />
+            <PostLoop type={pathPrefix} loop={group} />
 
-            <nav className="Pagination container">
-                <div className="prev">
-                    <NavLink test={first} url={previousUrl} text="Go to Previous Page" />
-                </div>
-                <div className="next">
-                    <NavLink test={last} url={nextUrl} text="Go to Next Page" />
-                </div>
-            </nav>
+            <Flex as="nav" justifyContent="space-between">
+                <StyledBox test={first} p={3} width={[1/2,1/2,1/2]} ariaLabel="prev">
+                    <NavLink test={first} url={previousUrl} text="Previous Page" />
+                </StyledBox>
+                <StyledBox test={last} textAlign="right" p={3} width={[1 / 2, 1 / 2, 1 / 2]} sx={{ borderLeft: "1px solid black" }} ariaLabel="next">
+                    <NavLink test={last} url={nextUrl} text="Next Page" />
+                </StyledBox>
+            </Flex>
         </Layout>
     );
 };
