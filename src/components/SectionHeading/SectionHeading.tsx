e7bb56e881246c379167399484304d352925910b
Ryosuke <ryosuke.san.hana@gmail.com>
2019-10-09 13:55:33 -0700

    :hammer: Pass props to Box ++ new "tall" props

--- src/components/SectionHeading/SectionHeading.tsx
+++ src/components/SectionHeading/SectionHeading.tsx
@@ -8,11 +8,11 @@ interface ISectionHeadingProps {
   heading: string,
 }
 
-const SectionHeading: React.FunctionComponent<ISectionHeadingProps> = ({className, emoji, heading, subheader}) => {
+const SectionHeading: React.FunctionComponent<ISectionHeadingProps> = ({className, emoji, heading, subheader, ...props}) => {
   return(
-  <Box className={className}>
+  <Box className={className} {...props}>
     <Heading variant="heading">{emoji && emoji} {heading}</Heading>
-    {subheader && <Heading variant="subheader" mt={2} pl={4}>{subheader}</Heading>}
+    {subheader && <Heading variant="subheader" mt={2} pl={4} mb={0}>{subheader}</Heading>}
   </Box>
   )
 };
@@ -21,6 +21,10 @@ const SectionHeading: React.FunctionComponent<ISectionHeadingProps> = ({classNam
 const StyledSectionHeading = styled(SectionHeading)`
   padding:2rem 1rem;
   border-bottom:1px solid ${(props) => props.theme.colors.black};
+
+  ${props => props.tall && `
+    padding-top:7rem;
+  `}
 `
 
 export default StyledSectionHeading;
