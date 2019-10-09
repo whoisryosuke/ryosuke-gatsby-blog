8607fb4cfc9db23643e8b52509ce9c6804ffabcc
Ryosuke <ryosuke.san.hana@gmail.com>
2019-10-09 16:18:27 -0700

    :bug: linked up actual image

--- src/components/Card/ImageCard.tsx
+++ src/components/Card/ImageCard.tsx
@@ -4,27 +4,19 @@ import { Flex, Heading, Image } from 'rebass/styled-components'
 import BaseCard from "./BaseCard"
 import Avatar from "../../assets/img/ryosuke-avatar-128.png"
 
-const StyledSubheader = styled(Heading)`
-  text-align:right;
-
-  @media(max-width:${(props) => props.theme.breakpoints[0]}) {
-    text-align:left;
-  }
-`
-
 interface IImageCardProps {
   title: string,
   subheader: string,
-  description: string
+  image: string
 }
 
-const ImageCard: React.FunctionComponent<IImageCardProps> = ({title, subheader, ...props}) => {
+const ImageCard: React.FunctionComponent<IImageCardProps> = ({title, subheader, image, ...props}) => {
   return(
     <BaseCard {...props}>
-      <Image src={Avatar} width="100%" height="auto" variant="avatar" sx={{marginBottom:"3rem"}} />
+      <Image src={image} width="100%" height="auto" variant="avatar" sx={{marginBottom:"3rem"}} />
       <Flex flexWrap='wrap'>
-        <Heading variant="header" mb="4" width={[1, 1/2]}>{title}</Heading>
-        <StyledSubheader variant="subheader" mb="4" width={[1, 1/2]}>{subheader}</StyledSubheader>
+        <Heading variant="subheader" mb="4" width={[1, 1, 1/2]}>{title}</Heading>
+        <Heading variant="label" mb="4" width={[1, 1, 1/2]} textAlign={['left','left','right']}>{subheader}</Heading>
       </Flex>
     </BaseCard>
   )
