96c851f015d4cf1e5ed256c2b809c3870cf95515
Ryosuke <ryosuke.san.hana@gmail.com>
2019-10-10 12:13:28 -0700

    :heavy_plus_sign: Debounce helper

---
+++ src/helpers/debounce.js
@@ -0,0 +1,11 @@
+export default function debounce(fn, delay) {
+  var timer = null
+  return function () {
+    var context = this,
+      args = arguments
+    clearTimeout(timer)
+    timer = setTimeout(function () {
+      fn.apply(context, args)
+    }, delay)
+  }
+}
