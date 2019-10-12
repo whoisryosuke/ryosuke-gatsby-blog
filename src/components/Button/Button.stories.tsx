1b798de38d323f68cfb083dc28909069df2b2e7d
Ryosuke <ryosuke.san.hana@gmail.com>
2019-10-11 20:11:09 -0700

    :white_check_mark: more button states

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
-import { Flex } from 'rebass/styled-components'
+import { Flex, Box, Button, Heading, Text } from 'rebass/styled-components'
 import ButtonOutline from "./ButtonOutline"
 
 export default { title: 'Button' };
 
-export const outline = () => <ButtonOutline>Test</ButtonOutline>
+export const outline = () => <ButtonOutline>Go to blog</ButtonOutline>
+export const filled = () => <Button><Text variant="label">Submit</Text></Button>
+export const filledBigger = () => <Button><Heading variant="subheader" mb={0}>Submit</Heading></Button>
+export const inline = () => <Flex>
+  <Box
+    as="a"
+    width={[1 / 2]}
+    href={`http://twitter.com/share`}
+  >
+    <ButtonOutline width={1} sx={{ borderRight: 0 }}>
+      Discuss on Twitter
+        </ButtonOutline>
+  </Box>
+  <Box
+    as="a"
+    width={[1 / 2]}
+    href={`http://www.tumblr.com/share/link?url=http://whoisryosuke.com`}
+  >
+    <ButtonOutline width={1}>Discuss on Tumblr</ButtonOutline>
+  </Box>
+</Flex>
