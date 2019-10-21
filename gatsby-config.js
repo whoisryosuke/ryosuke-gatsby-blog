c20b8371615f12123e9f0b7c64163b68b2267f08
Ryosuke <ryosuke.san.hana@gmail.com>
2019-10-21 11:41:13 -0700

    :wrench: SVG trace for images

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,7 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1080,
+              tracedSVG: true,
             },
           },
         ],
