94eb67720696d27e54461ec57d1410ef288aeab0
Ryosuke <ryosuke.san.hana@gmail.com>
2020-01-15 13:33:45 -0800

    :sparkles: dark mode featured bg

--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Box } from 'rebass/styled-components'
-import TriangleBG from '../../assets/svg/bg-triangle.svg'
+import TriangleBG from '@assets/svg/bg-triangle.svg'
+import TriangleBGDark from '@assets/svg/bg-triangle-dark.svg'
 
 const StyledBox = styled(Box)`
   box-shadow:none;
@@ -10,15 +11,21 @@ const StyledBox = styled(Box)`
   background-size:200px 200px;
   position:relative;
   z-index:10;
+
+  ${props =>
+    props.selectedTheme === 'dark' &&
+    `
+  background-image:url(${TriangleBGDark});
+  `}
 `
 
 interface Props {
   children: JSX.Element
 }
 
-const Featured: React.FC<Props> = ({children}) => {
+const Featured: React.FC<Props> = ({children, selectedTheme}) => {
   return (
-    <StyledBox px={4} py={6}>
+    <StyledBox px={4} py={6} selectedTheme={selectedTheme}>
       {children}
     </StyledBox>
   )
