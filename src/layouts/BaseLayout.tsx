fb311599b9f270b9b7d6fca0bf26861ddc9a2733
Ryosuke <ryosuke.san.hana@gmail.com>
2020-05-26 13:56:11 -0700

    :bug: Max width for mega screens

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet'
 import config from '../config'
 import { Box } from 'rebass/styled-components'
 
-import MobileHeader from "../components/Header/MobileHeader"
+import MobileHeader from '../components/Header/MobileHeader'
 import Footer from '../components/Footer/Footer'
 
 import appleTouchIcon from '../../static/assets/favicon/apple-touch-icon.png'
@@ -13,19 +13,28 @@ import favicon16 from '../../static/assets/favicon/favicon-16x16.png'
 const BaseLayout = ({ children }) => (
   <div>
     <Helmet>
-      <title>{ config.title }</title>
-      <meta name="description" content={ config.description } />
-      <meta name="keywords" content={ config.keywords } />
-      <link rel="apple-touch-icon" sizes="180x180" href={ appleTouchIcon } />
-      <link rel="icon" type="image/png" sizes="32x32" href={ favicon32 } />
-      <link rel="icon" type="image/png" sizes="16x16" href={ favicon16 } />
+      <title>{config.title}</title>
+      <meta name="description" content={config.description} />
+      <meta name="keywords" content={config.keywords} />
+      <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+      <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+      <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
-      <link href="https://fonts.googleapis.com/css?family=Fira+Code:500|Poppins:500|Roboto:400,400i,700,700i&display=swap" rel="stylesheet" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Fira+Code:500|Poppins:500|Roboto:400,400i,700,700i&display=swap"
+        rel="stylesheet"
+      />
     </Helmet>
     <MobileHeader />
 
-    <Box as="section" pt="4.8rem" className="App">
+    <Box
+      as="section"
+      pt="4.8rem"
+      maxWidth="1920px"
+      margin="auto"
+      className="App"
+    >
       {children}
     </Box>
 
