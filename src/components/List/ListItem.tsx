c559823b5ce894eeabf292b37c70ca9b7b0bac92
Ryosuke <ryosuke.san.hana@gmail.com>
2020-06-05 11:39:49 -0700

    :bug: Can't enforce no margin

--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -12,16 +12,10 @@ const ListItem = ({ children, className }) => (
 
 const StyledListItem = styled(ListItem)`
   list-style: square;
-  margin-bottom: ${props => props.theme.space[3]}px;
-
-  &:last-child,
-  p:last-child,
-  div:last-child {
-    margin-bottom: 0;
-  }
+  margin-bottom: ${(props) => props.theme.space[3]}px;
 
   & ul {
-    margin-top: ${props => props.theme.space[3]}px;
+    margin-top: ${(props) => props.theme.space[3]}px;
     margin-bottom: 0;
     padding-bottom: 0;
   }
